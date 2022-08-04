15b31a9a7b70684766d8d9f36ba4d698309e5ec8
WebDevAman <amanpandeyfiverr@gmail.com>
2022-08-04 07:47:40 +0530

    build: page speed

--- components/Navbar/index.jsx
+++ components/Navbar/index.jsx
@@ -37,16 +37,18 @@ const Index = () => {
     console.log(page)
     useEffect(() => {
         if (typeof window != 'undefined') {
-            var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
-            (function () {
-                var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
-                s1.async = true;
-                s1.src = 'https://embed.tawk.to/5fa105ebe019ee7748f03b4b/default';
-                s1.charset = 'UTF-8';
-                s1.setAttribute('crossorigin', '*');
-                s0.parentNode.insertBefore(s1, s0);
-            })();
-            <script async defer src="https://cdn.boei.help/hello.js"></script>
+            setTimeout(() => {
+                var Tawk_API = Tawk_API || {}, Tawk_LoadStart = new Date();
+                (function () {
+                    var s1 = document.createElement("script"), s0 = document.getElementsByTagName("script")[0];
+                    s1.async = true;
+                    s1.src = 'https://embed.tawk.to/5fa105ebe019ee7748f03b4b/default';
+                    s1.charset = 'UTF-8';
+                    s1.setAttribute('crossorigin', '*');
+                    s0.parentNode.insertBefore(s1, s0);
+                })();
+                <script async defer src="https://cdn.boei.help/hello.js"></script>
+            }, 2000);
         }
     }, [])
     return (
