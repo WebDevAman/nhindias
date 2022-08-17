fa322b47b0693e2ddc553f1e732ea72a05f51f0a
WebDevAman <amanpandeyfiverr@gmail.com>
2022-08-17 11:54:22 +0530

    sitemap xml generated

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
-const config = {
-    siteUrl: process.env.SITE_URL || 'https://networkershome.com',
-    generateRobotsTxt: true, // (optional)
-    // ...other options
+module.exports = {
+    siteUrl: 'https://www.networkershome.com',
+    generateRobotsTxt: true
 }
 
-export default config
