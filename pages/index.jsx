a631b8ae55ba03b60bf38fe1ab19fb47d713881d
vikasswaminh <67453408+vikasswaminh@users.noreply.github.com>
2022-08-18 12:19:09 +0530

    Update index.jsx

--- pages/index.jsx
+++ pages/index.jsx
@@ -18,7 +18,7 @@ export default function Home() {
     <Layout>
       <Head>
         <title>NETWORKERS HOME - Best CCIE, Cyber security and Cloud Training</title>
-        <meta content="Best CCIE, Cloud and Cyber security training. Join World best training with 100% placement guarantee" name="description" />
+        <meta content="CCIE, Cloud and Cyber security training  with 100% placement guarantee, CCIE Training by experts" name="description" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className='flex flex-col mt-12 space-y-12 lg:space-y-20'>
